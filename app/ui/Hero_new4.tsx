fe05120df4b521b959abdae90453dfe0a821d741
dedkola <maiden.tech@gmail.com>
2025-10-30 15:24:46 +0200

    add demo8

--- app/ui/Hero_new4.tsx
+++ app/ui/Hero_new4.tsx
@@ -4,29 +4,31 @@
 
 export default function Hero_new4() {
     return (
-        <section className="bg-[#E1D4C7] flex justify-center items-center min-h-screen p-6">
-            <div className="max-w-6xl w-full bg-[#E1D4C7] rounded-2xl shadow-md overflow-hidden">
+        <section className="bg-[#E1D4C7] flex justify-center items-center  p-6">
+            <div className="max-w-8xl w-full bg-[#E1D4C7] rounded-2xl shadow-md overflow-hidden  ">
             {/* Image Background Section */}
                 <div
-                    className="relative h-[400px] md:h-[700px] lg:h-[900px] flex flex-col justify-start bg-cover bg-center"
+                    className="relative w-full aspect-[3/2] flex flex-col items-center justify-center bg-center bg-contain bg-no-repeat"
                     style={{
-                        backgroundImage: "url('/hires/005.png')", // replace with your own image path
+                        backgroundImage: "url('/hires/005.png')",
+                        backgroundRepeat: "no-repeat",
                     }}
                 >
-                    <h1 className="relative text-5xl text-amber-800 text-center mt-10 md:mt-16 drop-shadow-sm !font-thin">
-                        PSYCHO-SOUND<br />HEALING
+                    <h1 className="relative text-8xl text-amber-800 text-center mt-10 md:mt-16 drop-shadow-sm !font-thin">
+                        PSYCHO-SOUND HEALING
                     </h1>
+
+                    <div className=" text-center px-6 py-10">
+                        <p className="text-[#211101] text-lg md:text-xl mb-6">
+                            Healing through Sound, Psychology, and Presence
+                        </p>
+                        <button className="bg-[#E1D4C7] text-[#211101] px-6 py-3 rounded-md shadow hover:bg-[#e9d8ca] transition">
+                            Book a Session
+                        </button>
+                    </div>
                 </div>
 
-                {/* Lower Section */}
-                <div className="bg-[#F1E6DC] text-center px-6 py-10">
-                    <p className="text-[#211101] text-lg md:text-xl mb-6">
-                        Healing through Sound, Psychology, and Presence
-                    </p>
-                    <button className="bg-[#E1D4C7] text-[#211101] px-6 py-3 rounded-md shadow hover:bg-[#e9d8ca] transition">
-                        Book a Session
-                    </button>
-                </div>
+
             </div>
         </section>
     )
