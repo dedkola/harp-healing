07ff820182a166142a912d049ecbc391b1ab1162
dedkola <maiden.tech@gmail.com>
2025-10-30 16:42:19 +0200

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { openSans, lato, ebGaramond, raleway } from '@/app/ui/fonts';
+import { Analytics } from "@vercel/analytics/next"
+
 
 export const metadata: Metadata = {
   title: "Harp Healing",
@@ -14,7 +16,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${openSans.variable} ${lato.variable} ${ebGaramond.variable} ${raleway.variable}`}>
-      <body suppressHydrationWarning className={`antialiased`}>{children}</body>
+      <body suppressHydrationWarning className={`antialiased`}>{children}         <Analytics />
+      </body>
     </html>
   );
 }
