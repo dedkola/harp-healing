802aeabb1b87e80d20f2f2171edf1c2590bee00d
dedkola <maiden.tech@gmail.com>
2025-10-30 15:37:46 +0200

    fix about2

--- app/about2/page.tsx
+++ app/about2/page.tsx
@@ -6,18 +6,18 @@ export default function About2() {
       {/* Background image */}
         <Navbar1 />
       <div
-        className="absolute inset-0 -z-10 bg-center bg-no-repeat bg-cover"
+        className="absolute inset-0 -z-10 bg-center bg-no-repeat bg-cover aspect-[3/2] "
         style={{ backgroundImage: "url('/hires/005.png')" }}
       />
 
       {/* Soft beige gradient overlay */}
       <div className="absolute inset-0 -z-10 bg-gradient-to-b " />
 
-      <div className="mx-auto max-w-2xl px-6 py-32 sm:py-40 lg:py-48 text-center">
+      <div className="mx-auto max-w-2xl px-6  sm:py-4 lg:py-88 text-center aspect-[3/2]">
         <h1 className="text-5xl md:text-6xl tracking-wide mb-2">CRYSTAL HARP</h1>
         <h2 className="text-4xl md:text-5xl tracking-wide">HEALING</h2>
 
-        <div className="mt-10 flex items-center justify-center gap-x-6">
+        <div className="mt-8 flex items-center justify-center gap-x-6">
           <a
             href="#"
             className="rounded-md bg-amber-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-amber-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-600"
