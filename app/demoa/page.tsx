b16a89ff0fd6cd788836ffe217b002e3ab7d73dd
dedkola <maiden.tech@gmail.com>
2025-10-31 12:17:57 +0200

    fix demo7 add demoa fix font

---
+++ app/demoa/page.tsx
@@ -0,0 +1,10 @@
+import {Navbar4} from '@/app/ui/Navbar4'
+import Hero_new3 from '@/app/ui/Hero_new3'
+export default function Page() {
+    return (
+        <div className="min-h-screen bg-[#F2EAE0]">
+            <Navbar4 />
+            <Hero_new3 />
+        </div>
+    )
+}
