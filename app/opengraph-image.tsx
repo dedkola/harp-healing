234caaaa087e41327e00f3cb0ffea93f0555c080
dedkola <maiden.tech@gmail.com>
2025-10-31 15:28:18 +0200

    opengraph

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -3,7 +3,7 @@ import { join } from 'node:path'
 import { readFile } from 'node:fs/promises'
 
 export default async function Image() {
-    const logoData = await readFile(join(process.cwd(), 'logo.png'), 'base64')
+    const logoData = await readFile(join(process.cwd(), '/logo.png'), 'base64')
     const logoSrc = `data:image/png;base64,${logoData}`
 
     return new ImageResponse(
