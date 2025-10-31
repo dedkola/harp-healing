0bf3a07021cee628f4f9dc20a2c7f251f6f8cdaa
dedkola <maiden.tech@gmail.com>
2025-10-31 15:15:56 +0200

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,17 @@ import { SpeedInsights } from '@vercel/speed-insights/next';
 export const metadata: Metadata = {
   title: "Harp Healing",
   description: "Harp Healing Welness",
+    icons: {
+        icon: [
+            { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+            { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+            { url: '/favicon-192x192.png', sizes: '192x192', type: 'image/png' },
+            { url: '/favicon.ico' }
+        ],
+        apple: [
+            { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
+        ]
+    }
 };
 
 export default function RootLayout({
