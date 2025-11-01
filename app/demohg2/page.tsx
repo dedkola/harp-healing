3b6944c29e09392189f702ccef068729cb335aed
dedkola <maiden.tech@gmail.com>
2025-11-01 13:02:32 +0200

    add demohg2

---
+++ app/demohg2/page.tsx
@@ -0,0 +1,17 @@
+import {Navbar4hg2} from '@/app/ui/Navbar4hg2'
+import Herohg2 from '@/app/ui/Hero_new4hg2'
+
+export default function Page() {
+    return (
+        <>
+        <div className=" ">
+            {/* Page wrapper to limit the page/body width to max 1000px only for this page */}
+            <Navbar4hg2 />
+            <div className="h-px bg-gradient-to-r from-transparent via-[#c19a6b]/40 to-transparent mt-10"></div>
+
+            
+        </div>
+        <Herohg2 />
+    </>
+    )
+}
